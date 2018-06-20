91737535774eec3023261a2075949f0be1b46dd0
unknown <ftd@qad.com>
2018-06-20 23:07:21 +0800

    Update advanced testing

--- AdvancedFunc.py
+++ AdvancedFunc.py
@@ -4,9 +4,12 @@ Created on Jun 20, 2018
 @author: ftd
 '''
 from builtins import range
+from _collections_abc import Iterable
+import os 
+import MyDef
 
 def my_slice():
-    sl = list(range(100))
+    sl = list(range(50, 100))
     s2 = tuple(range(20))
     s3 = 'ABCDEFGHIJKLMN'
     print(sl[12:17]) #from 12 to 17 in list
@@ -16,6 +19,67 @@ def my_slice():
     print(s3[3:8]) #from 3 to 8 in string
     print(s3[::3]) #each 3 record in string
     
+def my_interator():
+    s1 = {'a':'ftd', 'b':'pdj', 'c':'zal', 'd':'ynh'} #iterator for dict
+    if isinstance(s1, Iterable):
+        for sx1 in s1: #loop the keys
+            print(sx1)
+        for sx2 in s1.values(): #loop the values
+            print(sx2)
+        for sx3 in s1.items():
+            print(sx3)
+        for i, x in enumerate(s1): #loop with subscript
+            print(i, x)
 
+def my_listComp():
+    l1 = [n*5 for n in range(11)] #list generate comprehension
+    print(l1)
     
-my_slice()
+    l2 = [n*n for n in range(1, 10) if n % 2 == 0]
+    print(l2)
+    
+    l3 = [n+m for n in 'ABCD' for m in 'XYZ']
+    print(l3)
+    
+    l4 = [d for d in os.listdir('.')] #list all the files in current folder
+    print(l4)
+    
+    s1 = {'a':'ftd', 'b':'pdj', 'c':'zal', 'd':'ynh'} 
+    l5 = [n + '=' + m for n, m in s1.items()]
+    print(l5)
+
+def my_generator():
+    l1 = [n*5 for n in range(11)]
+    print(l1)
+    
+    l2 = (n*5 for n in range(11))
+    for n in l2:
+        print(n)
+
+
+
+
+
+
+
+
+
+
+
+#Main block
+tp = int(input('Test type (1 - 9): '))
+print(tp)
+
+if tp == 1:
+    my_slice()
+elif tp == 2:
+    my_interator()
+elif tp == 3:
+    my_listComp()
+elif tp == 4:
+    my_generator()
+elif tp == 5:
+    fir = int(input('Fibonacci start from: '))
+    maxC = int(input('Fibonacci loop for: '))
+    MyDef.my_Fibonacci(fir = fir, maxC = maxC)
+
--- MyDef.py
+++ MyDef.py
@@ -28,3 +28,13 @@ def my_power(x, n=2):
         n = n - 1
         s = s * x
     return s
+
+def my_Fibonacci(*, fir, maxC):
+    n, a = 0, 0
+    while n < maxC:
+        print(fir)
+        a, fir = fir, fir + a
+        n = n + 1
+    print('done')
+        
+    
