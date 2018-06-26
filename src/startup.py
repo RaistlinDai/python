25bd354055bf88b8ef7a06f44165d9c526c2f763
RaistlinDai <40423942+RaistlinDai@users.noreply.github.com>
2018-06-26 09:47:47 +0800

    Add a startup class, and remove the prevous test01

--- src/startup.py
+++ src/startup.py
@@ -1,9 +1,8 @@
 '''
-Created on Jun 20, 2018
+Created on Jun 26, 2018
 
 @author: ftd
 '''
 from src.main.pydev.com.ftd.generalutilities.metadata.gui.impl.Screen_file_load import Screen_file_load
-
 
 if __name__ == '__main__': Screen_file_load().mainloop()
