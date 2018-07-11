3d431df92138770c08110907b9251c22e98ca8e5
unknown <ftd@qad.com>
2018-07-11 21:09:39 +0800

    merge java_processor

--- src/main/pydev/com/ftd/generalutilities/metadata/service/javaproc/Java_processor.py
+++ src/main/pydev/com/ftd/generalutilities/metadata/service/javaproc/Java_processor.py
@@ -1,21 +1,54 @@
 '''
-Created on Jul 10, 2018
+Created on Jul 11, 2018
 
 @author: ftd
 '''
-from jpype import *
 import jpype
+import os
+from src.main.pydev.com.ftd.generalutilities.metadata.service.base.File_constant import File_constant
+from jpype._jexception import JavaException
 
 class Java_processor(object):
     '''
-    classdocs
+    before compiling this class, you have to install jpype and numpy in Python
     '''
 
-
     @staticmethod
-    def jar_reader():
-        # jvmPath = jpype.getDefaultJVMPath() 
-        jvmPath = ur'D:\jre-8u151-windows-i586\jre1.8.0_151\bin\client\jvm.dll'
+    def java__tester():
+        jvmPath = jpype.getDefaultJVMPath() 
         jpype.startJVM(jvmPath) 
-        jpype.java.lang.System.out.println("hello world!")
+        jpype.java.lang.System.out.println('hello world!')
         jpype.shutdownJVM()
+        
+    
+    @staticmethod
+    def java_3rd_tester():
+        fileconstant = File_constant()
+        #--- get the 3rd lib jar path
+        curr_path = os.path.dirname(os.path.abspath(__file__))
+        proj_path = curr_path[:curr_path.index(fileconstant.MY_PROJECT_PACKAGE)]
+        jarpath = proj_path + fileconstant.MY_PROJECT_3RD_LIB_PATH
+        
+        #--- start JVM and point out the jar path
+        if not jpype.isJVMStarted():  
+            jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.class.path=%s" % jarpath)
+            
+        try:
+            #--- import the jar class
+            javaclass = jpype.JClass('test.reader.ftdTest')
+            #--- create instance
+            javaInstance = javaclass('test in python')
+            #--- run java method
+            result = javaInstance.getFtdTest('FTD', jpype.java.lang.Integer(45))
+            print('java result: %s' % result)
+        except JavaException as ex: 
+            print("Caught exception : ", ex.message())
+        except:
+            print('Unkonw Error')
+        finally:
+            #--- close JVM
+            jpype.shutdownJVM()
+        
+    
+    
+    
