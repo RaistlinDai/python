e34094b661edee71bd71e261239a2a240e194149
RaistlinDai <40423942+RaistlinDai@users.noreply.github.com>
2018-08-01 17:34:20 +0800

    change layout of Frame_projfile_check, and add ignore functionality

--- src/main/pydev/com/ftd/generalutilities/metadata/gui/impl/frame/Frame_projfile_check.py
+++ src/main/pydev/com/ftd/generalutilities/metadata/gui/impl/frame/Frame_projfile_check.py
@@ -11,7 +11,7 @@ from src.main.pydev.com.ftd.generalutilities.metadata.service.fileproc.Xmlfile_p
 from src.main.pydev.com.ftd.generalutilities.metadata.service.base.File_constant import File_constant
 from src.main.pydev.com.ftd.generalutilities.metadata.service.base.File_processor import File_processor
 from src.main.pydev.com.ftd.generalutilities.metadata.service.fileproc.Java_processor import Java_processor
-from tkinter.messagebox import showerror
+from tkinter.messagebox import showerror, showwarning
 from src.main.pydev.com.ftd.generalutilities.metadata.service.multproc.Multiple_processor import Multiple_processor
 
 class Frame_projfile_check(FormatableFrame):
@@ -34,89 +34,97 @@ class Frame_projfile_check(FormatableFrame):
         self.__frame1 = FormatableFrame(self)
         self.__frame1.pack(side=TOP)
         #Title
-        self.__label01 = Label(self.__frame1, text="Project file checking", width= 45)
+        self.__label01 = Label(self.__frame1, text="Important files checking", width= 45)
         self.__label01.pack(side=TOP, fill=X, ipady=10)
         
         #---- panel 01 ----------
-        canv1 = Canvas(self, height=180, width=550)
+        canv1 = Canvas(self, height=120, width=550)
         #label
-        self.__label02 = Label(canv1, text='Metadata xml checking:')
-        self.__label02.place(height=20, width=180, relx=0.01, rely=0.01)
-        #selected details
-        self.__checkval01 = IntVar()
-        self.__checkbox01 = Checkbutton(canv1, text = "View metadata", variable = self.__checkval01, onvalue = 1, offvalue = 0, state=DISABLED)
-        self.__checkbox01.place(height=20, width=110, relx=0.1, rely=0.15)
-        self.__label03 = Label(canv1, text='Status :')
-        self.__label03.place(height=20, width=60, relx=0.4, rely=0.15)
-        self.__label04 = Label(canv1, text='< no need >')
-        self.__label04.place(height=20, width=120, relx=0.5, rely=0.15)
-        self.__button01 = Button(canv1, text="Detail")
-        self.__button01.place(height=20, width=50, relx=0.8, rely=0.15)
-        
-        self.__checkval02 = IntVar()
-        self.__checkbox02 = Checkbutton(canv1, text = "Resource metadata", variable = self.__checkval02, onvalue = 1, offvalue = 0, state=DISABLED)
-        self.__checkbox02.place(height=20, width=130, relx=0.1, rely=0.27)
-        self.__label05 = Label(canv1, text='Status :')
-        self.__label05.place(height=20, width=60, relx=0.4, rely=0.27)
-        self.__label06 = Label(canv1, text='< no need >')
-        self.__label06.place(height=20, width=120, relx=0.5, rely=0.27)
-        self.__button02 = Button(canv1, text="Detail")
-        self.__button02.place(height=20, width=50, relx=0.8, rely=0.27)
-        
+        self.__label02 = Label(canv1, text='Project file checking:')
+        self.__label02.place(height=20, width=150, relx=0.01, rely=0.02)
+        #selected details        
         self.__checkval03 = IntVar()
         self.__checkbox03 = Checkbutton(canv1, text = "Pom", variable = self.__checkval03, onvalue = 1, offvalue = 0, state=DISABLED)
-        self.__checkbox03.place(height=20, width=50, relx=0.1, rely=0.39)
+        self.__checkbox03.place(height=20, width=50, relx=0.1, rely=0.2)
         self.__label07 = Label(canv1, text='Status :')
-        self.__label07.place(height=20, width=60, relx=0.4, rely=0.39)
+        self.__label07.place(height=20, width=60, relx=0.4, rely=0.2)
         self.__label08 = Label(canv1, text='< no need >')
-        self.__label08.place(height=20, width=120, relx=0.5, rely=0.39)
+        self.__label08.place(height=20, width=120, relx=0.5, rely=0.2)
         self.__button03 = Button(canv1, text="Detail")
-        self.__button03.place(height=20, width=50, relx=0.8, rely=0.39)
+        self.__button03.place(height=20, width=50, relx=0.8, rely=0.2)
         
         self.__checkval04 = IntVar()
         self.__checkbox04 = Checkbutton(canv1, text = "beans-app-context", variable = self.__checkval04, onvalue = 1, offvalue = 0, state=DISABLED)
-        self.__checkbox04.place(height=20, width=125, relx=0.1, rely=0.51)
+        self.__checkbox04.place(height=20, width=125, relx=0.1, rely=0.4)
         self.__label09 = Label(canv1, text='Status :')
-        self.__label09.place(height=20, width=60, relx=0.4, rely=0.51)
+        self.__label09.place(height=20, width=60, relx=0.4, rely=0.4)
         self.__label10 = Label(canv1, text='< no need >')
-        self.__label10.place(height=20, width=120, relx=0.5, rely=0.51)
+        self.__label10.place(height=20, width=120, relx=0.5, rely=0.4)
         self.__button04 = Button(canv1, text="Detail")
-        self.__button04.place(height=20, width=50, relx=0.8, rely=0.51)
+        self.__button04.place(height=20, width=50, relx=0.8, rely=0.4)
         
         self.__checkval05 = IntVar()
         self.__checkbox05 = Checkbutton(canv1, text = "entityMap", variable = self.__checkval05, onvalue = 1, offvalue = 0, state=DISABLED)
-        self.__checkbox05.place(height=20, width=80, relx=0.1, rely=0.63)
+        self.__checkbox05.place(height=20, width=80, relx=0.1, rely=0.6)
         self.__label11 = Label(canv1, text='Status :')
-        self.__label11.place(height=20, width=60, relx=0.4, rely=0.63)
+        self.__label11.place(height=20, width=60, relx=0.4, rely=0.6)
         self.__label12 = Label(canv1, text='< no need >')
-        self.__label12.place(height=20, width=120, relx=0.5, rely=0.63)
+        self.__label12.place(height=20, width=120, relx=0.5, rely=0.6)
         self.__button05 = Button(canv1, text="Detail")
-        self.__button05.place(height=20, width=50, relx=0.8, rely=0.63)
+        self.__button05.place(height=20, width=50, relx=0.8, rely=0.6)
         
         #warning message in xml panel
         self.__label99 = Label(canv1, text='Warning : Please run svn revert/update if something in project is incorrect.', fg='red')
-        self.__label99.place(height=20, width=400, relx=0.1, rely=0.75)
+        self.__label99.place(height=20, width=400, relx=0.1, rely=0.8)
                 
         canv1.pack()
         
         #---- panel 02 ----------
         canv2 = Canvas(self, height=80, width=550)
         #labe2
-        label2 = Label(canv2, text='Jar checking:')
-        label2.place(height=20, width=100, relx=0.01, rely=0.05)
+        label2 = Label(canv2, text='Metadata checking:')
+        label2.place(height=20, width=120, relx=0.01, rely=0.05)
         
-        self.__chkint51 = IntVar()
-        self.__checkval51 = self.__chkint51
-        self.__checkbox51 = Checkbutton(canv2, text = "financials-impl", variable = self.__checkval51, onvalue = 1, offvalue = 0, state=DISABLED)
-        self.__checkbox51.place(height=20, width=110, relx=0.1, rely=0.35)
-        self.__label51 = Label(canv2, text='Status :')
-        self.__label51.place(height=20, width=60, relx=0.4, rely=0.35)
-        self.__label52 = Label(canv2, text='< no need >')
-        self.__label52.place(height=20, width=120, relx=0.5, rely=0.35)
-        self.__button51 = Button(canv2, text="Detail")
-        self.__button51.place(height=20, width=50, relx=0.8, rely=0.35)
+        self.__checkval01 = IntVar()
+        self.__checkbox01 = Checkbutton(canv2, text = "View metadata", variable = self.__checkval01, onvalue = 1, offvalue = 0, state=DISABLED)
+        self.__checkbox01.place(height=20, width=110, relx=0.1, rely=0.3)
+        self.__label03 = Label(canv2, text='Status :')
+        self.__label03.place(height=20, width=60, relx=0.4, rely=0.3)
+        self.__label04 = Label(canv2, text='< no need >')
+        self.__label04.place(height=20, width=120, relx=0.5, rely=0.3)
+        self.__button01 = Button(canv2, text="Detail")
+        self.__button01.place(height=20, width=50, relx=0.8, rely=0.3)
+        
+        self.__checkval02 = IntVar()
+        self.__checkbox02 = Checkbutton(canv2, text = "Resource metadata", variable = self.__checkval02, onvalue = 1, offvalue = 0, state=DISABLED)
+        self.__checkbox02.place(height=20, width=130, relx=0.1, rely=0.6)
+        self.__label05 = Label(canv2, text='Status :')
+        self.__label05.place(height=20, width=60, relx=0.4, rely=0.6)
+        self.__label06 = Label(canv2, text='< no need >')
+        self.__label06.place(height=20, width=120, relx=0.5, rely=0.6)
+        self.__button02 = Button(canv2, text="Detail")
+        self.__button02.place(height=20, width=50, relx=0.8, rely=0.6)
         
         canv2.pack()
+        
+        #---- panel 03 ----------
+        canv3 = Canvas(self, height=60, width=550)
+        #labe3
+        label3 = Label(canv3, text='Jar checking:')
+        label3.place(height=20, width=100, relx=0.01, rely=0.05)
+        
+        self.__chkint91 = IntVar()
+        self.__checkval91 = self.__chkint91
+        self.__checkbox91 = Checkbutton(canv3, text = "financials-impl", variable = self.__checkval91, onvalue = 1, offvalue = 0)
+        self.__checkbox91.place(height=20, width=110, relx=0.1, rely=0.5)
+        self.__label91 = Label(canv3, text='Status :')
+        self.__label91.place(height=20, width=60, relx=0.4, rely=0.5)
+        self.__label92 = Label(canv3, text='< no need >')
+        self.__label92.place(height=20, width=120, relx=0.5, rely=0.5)
+        self.__button91 = Button(canv3, text="Detail")
+        self.__button91.place(height=20, width=50, relx=0.8, rely=0.5)
+        
+        canv3.pack()
         
         #--- initialize
         self.verify_proj_files()
@@ -152,13 +160,13 @@ class Frame_projfile_check(FormatableFrame):
             newlabel01 = "< passed >"
             self.__label04.config(text=newlabel01, fg='blue')
             self.get_dtos().set_viewfullpath(viewfullpath)
-            self.__checkstatus['ViewMetadata'] = True
+            self.__checkstatus['ViewMetadata'] = [True, self.__checkval01]
         else:
             newlabel01 = "< failed >"
             self.__label04.config(text=newlabel01, fg='red')
             btnlabel = 'Correct'
             self.__button01.config(text=btnlabel)
-            self.__checkstatus['ViewMetadata'] = False
+            self.__checkstatus['ViewMetadata'] = [False, self.__checkval01]
         self.__checkval01.set(1)
         #bind button click event
         self.__button01.bind('<Button-1>', self.event_adapter(self.detal_button_click,
@@ -179,13 +187,13 @@ class Frame_projfile_check(FormatableFrame):
             newlabel = "< passed >"
             self.__label06.config(text=newlabel, fg='blue')
             self.get_dtos().set_resourcefullpath(resourcefullpath)
-            self.__checkstatus['ResourceMetadata'] = True
+            self.__checkstatus['ResourceMetadata'] = [True, self.__checkval02]
         else:
             newlabel = "< failed >"
             self.__label06.config(text=newlabel, fg='red')
             btnlabel = 'Correct'
             self.__button02.config(text=btnlabel)
-            self.__checkstatus['ResourceMetadata'] = False
+            self.__checkstatus['ResourceMetadata'] = [False, self.__checkval02]
         self.__checkval02.set(1)
         self.__button02.bind('<Button-1>', self.event_adapter(self.detal_button_click,
                                                               iscorrect=self.__checkstatus['ResourceMetadata'],
@@ -203,13 +211,13 @@ class Frame_projfile_check(FormatableFrame):
             newlabel = "< passed >"
             self.__label08.config(text=newlabel, fg='blue')
             self.get_trans().set_pomDto(pomDto)
-            self.__checkstatus['Pom'] = True
+            self.__checkstatus['Pom'] = [True, self.__checkval03]
         else:
             newlabel = "< failed >"
             self.__label08.config(text=newlabel, fg='red')
             btnlabel = 'Correct'
             self.__button03.config(text=btnlabel)
-            self.__checkstatus['Pom'] = False
+            self.__checkstatus['Pom'] = [False, self.__checkval03]
         self.__checkval03.set(1)
         self.__button03.bind('<Button-1>', self.event_adapter(self.detal_button_click,
                                                               iscorrect=self.__checkstatus['Pom'],
@@ -225,13 +233,13 @@ class Frame_projfile_check(FormatableFrame):
             newlabel = "< passed >"
             self.__label10.config(text=newlabel, fg='blue')
             self.get_trans().set_pomDto(pomDto)
-            self.__checkstatus['beans-app-context'] = True
+            self.__checkstatus['beans-app-context'] = [True, self.__checkval04]
         else:
             newlabel = "< failed >"
             self.__label10.config(text=newlabel, fg='red')
             btnlabel = 'Correct'
             self.__button04.config(text=btnlabel)
-            self.__checkstatus['beans-app-context'] = False
+            self.__checkstatus['beans-app-context'] = [False, self.__checkval04]
         self.__checkval04.set(1)
         self.__button04.bind('<Button-1>', self.event_adapter(self.detal_button_click,
                                                               iscorrect=self.__checkstatus['beans-app-context'],
@@ -247,13 +255,13 @@ class Frame_projfile_check(FormatableFrame):
             newlabel = "< passed >"
             self.__label12.config(text=newlabel, fg='blue')
             self.get_trans().set_pomDto(pomDto)
-            self.__checkstatus['entityMap'] = True
+            self.__checkstatus['entityMap'] = [True, self.__checkval05]
         else:
             newlabel = "< failed >"
             self.__label12.config(text=newlabel, fg='red')
             btnlabel = 'Correct'
             self.__button05.config(text=btnlabel)
-            self.__checkstatus['entityMap'] = False
+            self.__checkstatus['entityMap'] = [False, self.__checkval05]
         self.__checkval05.set(1)
         self.__button05.bind('<Button-1>', self.event_adapter(self.detal_button_click,
                                                               iscorrect=self.__checkstatus['entityMap'],
@@ -278,24 +286,24 @@ class Frame_projfile_check(FormatableFrame):
             
             if result:
                 newlabel = "< passed >"
-                self.__label52.config(text=newlabel, fg='blue')
+                self.__label92.config(text=newlabel, fg='blue')
                 self.get_trans().set_finImplJarPath(jarfullpath)
-                self.__checkstatus['JAR'] = True
+                self.__checkstatus['JAR'] = [True, self.__checkval91]
             else:
                 newlabel = "< failed >"
-                self.__label52.config(text=newlabel, fg='red')
+                self.__label92.config(text=newlabel, fg='red')
                 btnlabel = 'Correct'
-                self.__button51.config(text=btnlabel)
-                self.__checkstatus['JAR'] = False
+                self.__button91.config(text=btnlabel)
+                self.__checkstatus['JAR'] = [False, self.__checkval91]
         else:
             newlabel = "< passed >"
-            self.__label52.config(text=newlabel, fg='blue')
+            self.__label92.config(text=newlabel, fg='blue')
             self.__checkstatus['JAR'] = True
             #jar name
             jarname = File_processor.get_file_name(self.get_trans().get_finImplJarPath())
                 
-        self.__checkval51.set(1)
-        self.__button51.bind('<Button-1>', self.event_adapter(self.detal_button_click,
+        self.__checkval91.set(1)
+        self.__button91.bind('<Button-1>', self.event_adapter(self.detal_button_click,
                                                               iscorrect=self.__checkstatus['JAR'],
                                                               filename=jarname,
                                                               filetype='JAR',
@@ -333,11 +341,11 @@ class Frame_projfile_check(FormatableFrame):
                     self.get_trans().set_finImplJarPath(result_dto['filepath'])
                     #update labels
                     newlabel = "< passed >"
-                    self.__label52.config(text=newlabel, fg='blue')
+                    self.__label92.config(text=newlabel, fg='blue')
                     btnlabel = 'Detail'
-                    self.__button51.config(text=btnlabel)
+                    self.__button91.config(text=btnlabel)
                     #update event parameters
-                    self.__button51.bind('<Button-1>', self.event_adapter(self.detal_button_click,
+                    self.__button91.bind('<Button-1>', self.event_adapter(self.detal_button_click,
                                                               iscorrect=self.__checkstatus['JAR'],
                                                               filename=result_dto['filename'],
                                                               filetype='JAR',
@@ -349,12 +357,15 @@ class Frame_projfile_check(FormatableFrame):
         verify the input directory
         '''
         for flag in self.__checkstatus.values():
-            if not flag:
+            if not flag[0] and flag[1].get() == 1:
                 showerror('Error', 'There are errors existing, please correct it!')
                 return False
+            elif flag[1].get() == 0:
+                showwarning('Warning', 'Some generators could not work because the related file validations are skipped.')
+                break
         
         #jar processing
-        if self.__chkint51.get() == 1:
+        if self.__chkint91.get() == 1:
             Multiple_processor.multiprocessing_service(self.get_trans())
         
         return True
